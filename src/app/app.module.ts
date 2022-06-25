77944af47dacc2b39e0740783fff83224d60a906
Shivam <mehtashivam49@gmail.com>
2022-06-25 18:23:23 +0530

    screen ui done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReferencesComponent } from './references/references.component';
+import { ReferenceComponentComponent } from './reference-component/reference-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReferencesComponent
+    ReferencesComponent,
+    ReferenceComponentComponent
   ],
   imports: [
     BrowserModule,
