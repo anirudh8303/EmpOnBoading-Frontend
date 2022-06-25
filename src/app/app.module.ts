e0acbc148497c290c1ab22c40f995bfabc5c064e
anirudh8303 <anirudhsharma8303@gmail.com>
2022-06-25 13:20:20 +0530

    Initial Setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReferencesComponent } from './references/references.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReferencesComponent
   ],
   imports: [
     BrowserModule,
