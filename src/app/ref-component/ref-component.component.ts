67febebd2b454b83911c26dce245acaa100cbf31
Ishikabindal-56 <ishikabindal56@gmail.com>
2022-06-27 16:44:51 +0530

    validations done

--- src/app/ref-component/ref-component.component.ts
+++ src/app/ref-component/ref-component.component.ts
@@ -8,8 +8,6 @@ declare var $: any;
   styleUrls: ['./ref-component.component.css'],
 })
 export class RefComponentComponent implements OnInit {
-
-  
   constructor() {}
 
   ngOnInit(): void {}
@@ -20,13 +18,19 @@ export class RefComponentComponent implements OnInit {
       Validators.maxLength(50),
       Validators.pattern('^[a-zA-Z ]*$'),
     ]),
-    email: new FormControl('', [Validators.email, Validators.required]),
+    email: new FormControl('', [
+      Validators.email, 
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+    ]),
     mobile: new FormControl('', [
       Validators.required,
       Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
     ]),
-    desg: new FormControl('', [Validators.required,
-      Validators.pattern('^[a-zA-Z ]*$')]),
+    desg: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z ]*$'),
+    ]),
     company: new FormControl('', [Validators.required]),
   });
   get name() {
@@ -44,5 +48,4 @@ export class RefComponentComponent implements OnInit {
   get company() {
     return this.contactForm.get('company');
   }
-  
 }
