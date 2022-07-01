baea6e77de837d7db7694f6dcaa6a6ae07812404
Ishikabindal-56 <ishikabindal56@gmail.com>
2022-07-01 11:59:30 +0530

    form updated

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-references',
@@ -10,6 +11,43 @@ export class ReferencesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  contactForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50),
+      Validators.pattern('^[a-zA-Z ]*$'),
+    ]),
+    email: new FormControl('', [
+      Validators.email, 
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+    ]),
+    mobile: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+    ]),
+    desg: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z ]*$'),
+    ]),
+    company: new FormControl('', [Validators.required]),
+  });
+  get name() {
+    return this.contactForm.get('name');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get mobile() {
+    return this.contactForm.get('mobile');
+  }
+  get desg() {
+    return this.contactForm.get('desg');
+  }
+  get company() {
+    return this.contactForm.get('company');
   }
 
 }
